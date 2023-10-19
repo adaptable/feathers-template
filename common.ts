43f3aea8c6e9c6b309d3f0e080740f8a492eaa74
Mark Terrel <mark@adaptable.io>
2023-10-19 16:37:42 -0600

    feat: Add nixpacks and Golang support

--- common.ts
+++ common.ts
@@ -10,15 +10,19 @@ export interface ConfigDomain {
 export type ConfigDomains = ConfigDomain[];
 
 export interface Config {
+    buildCommand?: string;
     buildEnvironment?: EnvSimple;
+    builderType?: "paketo" | "nixpacks";
     databaseType: "mongodb" | "mssql" | "none" | "postgres";
     domains: ConfigDomains;
     environment?: EnvSimple;
+    installCommand?: string;
     nodeRunScripts?: string;
     nodeStartScript?: "start";
     nodeVersion?: string;
     projectPath?: string;
     pythonVersion?: string;
+    repoSubdir?: string;
     startCommand?: string;
 }
 

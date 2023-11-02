23ada1150aa01ed01b7afb5b65dcda2519af3dde
Mark Terrel <mark@adaptable.io>
2023-11-02 13:43:27 -0600

    feat: Add dockerfile build support

--- startup/buildInfo.js
+++ startup/buildInfo.js
@@ -3,47 +3,43 @@
 
 const { loadAdaptableAppConfig } = require("@adaptable/template");
 
-/**
- * @type {import("../common").Config}
- */
-const appConfig = loadAdaptableAppConfig();
-
-const appId = process.env.ADAPTABLE_APP_ID;
-if (appId == null) throw new Error("No ADAPTABLE_APP_ID found");
-
-const revId = process.env.ADAPTABLE_APPREVISION_ID;
-if (revId == null) throw new Error("No ADAPTABLE_APPREVISION_ID");
-
-const tags = (process.env.ADAPTABLE_TEMPLATE_TAGS || "").split(",");
-
-let { builderType } = appConfig;
-
 // IMPORTANT: Update config.schema.json when the buildpack/nixpacks image changes
 // Node, Python, or other runtime versions.
 const defaultBuilderImage = "paketobuildpacks/builder:0.2.443-full";
 const oldBuilderImage = "paketobuildpacks/builder:0.2.6-full";
 const nixpacksVersion = "1.18.0";
 
-const userEnv = appConfig.buildEnvironment || {};
+/**
+ * @param {Record<string, string | undefined>} obj
+ * @returns {Record<string, string>}
+ */
+function stripUndef(obj) {
+    /** @type Record<string, string> */
+    const ret = {};
 
-if (tags.includes("go") || tags.includes("php")) {
-    if (builderType === "paketo") {
-        const msg = `${tags.join("+")} not currently supported with paketo builderType. Use nixpacks instead.`;
-        // eslint-disable-next-line no-console
-        console.log(msg);
-        throw new Error(msg);
-    }
-    builderType = "nixpacks";
-} else if (!builderType) {
-    builderType = "paketo";
+    Object.entries(obj).forEach(([key, val]) => {
+        if (val != null) ret[key] = val;
+    });
+
+    return ret;
 }
 
 /**
- * @type {import("@adaptable/client/dist/api-types/builds").CreateBuild}
+ * @typedef {import("../common").Config} AppConfig
+ * @typedef {import("@adaptable/client/dist/api-types/builds").CreateBuild} CreateBuild
+ * @typedef {CreateBuild["config"]} BuildConfig
+ *
+ * @typedef {object} BuildInfo
+ * @property {BuildConfig} config
+ * @property {Record<string, string | undefined>} env
  */
-let imageBuildProps;
 
-if (builderType === "paketo") {
+/**
+ * @param {AppConfig} appConfig
+ * @param {string[]} tags
+ * @returns {BuildInfo}
+ */
+function paketoBuilder(appConfig, tags) {
     /**
      * @type Record<string, string | undefined>
      */
@@ -57,20 +53,14 @@ if (builderType === "paketo") {
         BP_NODE_RUN_SCRIPTS: appConfig.nodeRunScripts || "",
         BP_NODE_VERSION: appConfig.nodeVersion,
         BP_CPYTHON_VERSION: appConfig.pythonVersion,
-        // User can override the top level settings
-        ...userEnv,
     };
 
-    imageBuildProps = {
-        appId,
-        config: {
-            type: "buildpack",
-            builder: defaultBuilderImage,
-        },
-        env,
-        imageName: "appimage",
-        plan: "hobby",
-        revId,
+    /**
+     * @type BuildConfig
+     */
+    const config = {
+        type: "buildpack",
+        builder: defaultBuilderImage,
     };
 
     // This works around the following issues:
@@ -86,13 +76,13 @@ if (builderType === "paketo") {
     if (tags.includes("nodejs")) {
         // Use the older builder for versions < 18
         if (["12", "14", "16"].includes(appConfig.nodeVersion || "")) {
-            imageBuildProps.config.builder = oldBuilderImage;
+            config.builder = oldBuilderImage;
 
             // Older builder errors if BP_NODE_RUN_SCRIPTS is the empty string
             if (!env.BP_NODE_RUN_SCRIPTS) delete env.BP_NODE_RUN_SCRIPTS;
         }
 
-        imageBuildProps.config.buildpacks = [
+        config.buildpacks = [
             "paketo-buildpacks/nodejs",
             // buildpack-launch is required for BP_LAUNCH_COMMAND
             "adaptable/buildpack-launch:0.0.7",
@@ -100,21 +90,43 @@ if (builderType === "paketo") {
     } else if (tags.includes("python")) {
         // Use the older builder for versions < 3.10
         if (["3.6", "3.7", "3.8", "3.9"].includes(appConfig.pythonVersion || "")) {
-            imageBuildProps.config.builder = oldBuilderImage;
+            config.builder = oldBuilderImage;
 
             // Workaround is only required for the older builder
-            imageBuildProps.config.preBuildScript = pythonWorkaround;
+            config.preBuildScript = pythonWorkaround;
         }
 
-        imageBuildProps.config.buildpacks = [
+        config.buildpacks = [
             "paketo-buildpacks/python",
             // buildpack-launch is required for BP_LAUNCH_COMMAND
             "adaptable/buildpack-launch:0.0.7",
         ];
     }
-} else {
-    // Nixpacks
 
+    return { config, env };
+}
+
+/**
+ * @param {AppConfig} appConfig
+ * @param {string[]} tags
+ * @returns {BuildInfo}
+ */
+function dockerfileBuilder(appConfig, tags) {
+    /** @type BuildConfig */
+    const config = {
+        type: "dockerfile",
+    };
+    if (appConfig.dockerfile) config.dockerfile = appConfig.dockerfile;
+
+    return { config, env: {} };
+}
+
+/**
+ * @param {AppConfig} appConfig
+ * @param {string[]} tags
+ * @returns {BuildInfo}
+ */
+function nixpacksBuilder(appConfig, tags) {
     let providers;
 
     if (tags.includes("nodejs")) providers = ["node"];
@@ -130,15 +142,9 @@ if (builderType === "paketo") {
         appConfig.startCommand = "perl /assets/prestart.pl /assets/nginx.template.conf /nginx.conf && (php-fpm -D -y /assets/php-fpm.conf && nginx -c /nginx.conf)";
     }
 
-    const setIfDefined = {
+    const variables = stripUndef({
         NIXPACKS_NODE_VERSION: appConfig.nodeVersion,
         NIXPACKS_PYTHON_VERSION: appConfig.pythonVersion,
-    };
-
-    const variables = {};
-
-    Object.entries(setIfDefined).forEach(([key, val]) => {
-        if (val != null) variables[key] = val;
     });
 
     /**
@@ -179,20 +185,76 @@ if (builderType === "paketo") {
         };
     }
 
-    imageBuildProps = {
-        appId,
+    return {
         config: {
             type: "nixpacks",
             version: nixpacksVersion,
             plan,
         },
-        env: userEnv,
+        env: {},
+    };
+}
+
+/**
+ * @returns {CreateBuild}
+ */
+function makeBuildProps() {
+    /**
+     * @type {import("../common").Config}
+     */
+    const appConfig = loadAdaptableAppConfig();
+
+    const appId = process.env.ADAPTABLE_APP_ID;
+    if (appId == null) throw new Error("No ADAPTABLE_APP_ID found");
+
+    const revId = process.env.ADAPTABLE_APPREVISION_ID;
+    if (revId == null) throw new Error("No ADAPTABLE_APPREVISION_ID");
+
+    const tags = (process.env.ADAPTABLE_TEMPLATE_TAGS || "").split(",");
+
+    let { builderType } = appConfig;
+
+    const userEnv = appConfig.buildEnvironment || {};
+
+    if (tags.includes("go") || tags.includes("php")) {
+        if (builderType === "paketo") {
+            const msg = `${tags.join("+")} not currently supported with paketo builderType. Use nixpacks instead.`;
+            // eslint-disable-next-line no-console
+            console.log(msg);
+            throw new Error(msg);
+        }
+        builderType = "nixpacks";
+    } else if (tags.includes("dockerfile")) {
+        builderType = "dockerfile";
+    } else if (!builderType) {
+        builderType = "paketo";
+    }
+
+    /**
+     * @type {BuildInfo}
+     */
+    // eslint-disable-next-line no-nested-ternary
+    const { config, env } = builderType === "paketo" ? paketoBuilder(appConfig, tags)
+        : builderType === "dockerfile" ? dockerfileBuilder(appConfig, tags)
+            : nixpacksBuilder(appConfig, tags);
+
+    /** @type {CreateBuild} */
+    const imageBuildProps = {
+        appId,
+        config,
+        env: {
+            ...stripUndef(env),
+            // User can override builder env
+            ...userEnv,
+        },
         imageName: "appimage",
         plan: "hobby",
         revId,
     };
+
+    if (appConfig.repoSubdir) imageBuildProps.subdir = appConfig.repoSubdir;
+
+    return imageBuildProps;
 }
 
-if (appConfig.repoSubdir) imageBuildProps.subdir = appConfig.repoSubdir;
-
-module.exports.imageBuildProps = imageBuildProps;
+module.exports.imageBuildProps = makeBuildProps();
